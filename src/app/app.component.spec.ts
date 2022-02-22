9afd3c2da273a4973f46856a8d97ef26e532b68d
Alex da Silva Ferreira <alex120999@hotmail.com.br>
2022-02-22 17:31:00 -0300

    Instalando angular e adicionando cabeçalho no app.component.html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gerenciador-de-tarefas app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Gerenciador de tarefas');
   });
 });
